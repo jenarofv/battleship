33b3fb45cea273c3d47da20f0d7bfea9517e209a
jenaro fv <jen4r0@gmail.com>
2026-02-18 21:17:10 -0600

    fix(eslint): modify config so eslint works

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["js/recommended", "prettier"],
+    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
 ]);
