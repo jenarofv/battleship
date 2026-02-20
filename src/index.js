ff24a9d44061ba4cd7e057a49086a58aeffc2eb1
jenaro fv <jen4r0@gmail.com>
2026-02-19 18:29:26 -0600

    content: add and style title and bg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+
+const body = document.querySelector("body");
+const title = document.createElement("h1");
+title.innerHTML = "Battleship";
+title.style.textTransform = "uppecase";
+body.appendChild(title);
