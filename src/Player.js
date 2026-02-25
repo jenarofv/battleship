136bff85ac5a98129bd0de2e834552c118deb635
jenaro fv <jen4r0@gmail.com>
2026-02-24 21:34:26 -0600

    fix: prevent board initialization from crashing
    inserts boats from largest to smallest

--- src/Player.js
+++ src/Player.js
@@ -23,13 +23,14 @@ class Player {
     if (numOfShips > 9) {
       throw new RangeError("too much ships!");
     }
-    let i = 2;
-    while (i <= numOfShips + 1) {
+    let i = 0;
+    while (i < numOfShips) {
       const randX = Math.floor(Math.random() * 10);
       const randY = Math.floor(Math.random() * 10);
       const vert = Math.random() > 0.5;
       try {
-        this.gameboard.addShip(i, [randX, randY], vert);
+        console.log(`adding ${numOfShips + 1 - i}-cell ship`);
+        this.gameboard.addShip(numOfShips + 1 - i, [randX, randY], vert);
         i++;
       } catch {}
     }
